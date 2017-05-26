be85e0277fb52a017cc7d23f9e7d2ed3a1ea730f
gn01249750 <hup@uw.edu>
2017-05-26 08:01:29 -0700

    from main to another

--- assignment0.rkt
+++ assignment0.rkt
@@ -1,4 +1,5 @@
 dddd:q;; The first three lines of this file were inserted by DrRacket. They record metadata
+dddd
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname assignment0) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ;; CS2500 
